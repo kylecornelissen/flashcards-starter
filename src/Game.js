4845a181456ad685774aa98e188ff2f7fe77ab93
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-11-21 16:22:24 -0700

    Add start and createCards methods to game class and clean up code tabs

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,33 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
 
 class Game {
-  constructor() {}
-
+  constructor() {
+  }
+  start() {
+    const deck = new Deck(this.createCards());
+    const round = new Round(deck);
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+  }
+  createCards() {
+    let randomCards = data.prototypeData.sort(function() { return 0.5 - Math.random() } ).slice(0,10);
+    let deckCards = []
+    randomCards.forEach(function(card) {
+      deckCards.push(new Card(card.id, card.question, card.answers, card.correctAnswer));
+    });
+    return deckCards
+  }
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
-
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
---
+++ test/Game-test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+
+describe('Game', function() {
+  beforeEach(function() {
+    global.game = new Game();
+  });
+
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', function() {
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('should be able to create random cards', function() {
+    randomDeck = game.createCards();
+
+    expect(randomDeck.length).to.deep.equal(10);
+  });
+});
