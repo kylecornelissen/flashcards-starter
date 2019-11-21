2993400eec02cc57ea8bd45dcbc599fff173bc4d
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-11-20 19:46:25 -0700

    Add function and instance tests for round class

---
+++ test/Round-test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+});
