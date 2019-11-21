41df3fe099f51b61e9a4275d1884c72aba54c871
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-11-20 22:21:03 -0700

    Add currentCard, turns, and incorrectGuesses properties to round class

--- src/Round.js
+++ src/Round.js
@@ -5,7 +5,11 @@ const util = require('./util');
 class Round {
   constructor(deck) {
     this.deck = deck;
+    this.currentCard = deck.cards[0];
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
+  
 }
 
 module.exports = Round;
